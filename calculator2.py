7e029c10c679e16b0767499da3eb2d4449ed0a41
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-23 22:10:59 +0800

    pseudocode for guide

--- calculator2.py
+++ calculator2.py
@@ -1,1 +1,21 @@
 # redo calculator with oop
+
+# ask user to choose math operations between "Addition", "Subtraction", "Multiplication", or "Division"
+# evaluate the chosen math operations
+    # if input is "Addition"
+        # ask for 2 numbers
+        # display result
+    # if input is "Subtraction"
+        # ask for 2 numbers
+        # display result
+    # if input is "Multiplication"
+        # ask for 2 numbers
+        # display result
+    # if input is "Division"
+        # ask for 2 numbers
+        # display result
+# ----------------------------------------------
+# ask user if wants to input again
+    # if yes
+    # if no
+        # display appreciation
