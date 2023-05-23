34365db669714464f8690c43fd4c7c9d30daf68f
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-23 23:16:36 +0800

    forgot to return again

--- user_display.py
+++ user_display.py
@@ -2,6 +2,9 @@
 class UserDisplay:
     def math_operator(self):
         user_operator = input("Please choose what math operation you need: 'Addition', 'Subtraction', 'Multiplication' or 'Division': ")
-        return
+        return user_operator
     def user_number(self):
         first_number = float(input("Please enter number: "))
+        return first_number
+    def display_addition(self, addition):
+        print("The sum is: " + str(addition))
