5648a4adeac8f1ab4c3f9fa7d6933885d442223d
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-23 22:52:35 +0800

    if addition

--- calculator2.py
+++ calculator2.py
@@ -2,17 +2,21 @@
 from user_display import UserDisplay
 from calculator_add import CalculatorAdd
 
-# ask user to choose math operations between "Addition", "Subtraction", "Multiplication", or "Division"
 calculator_operator2 = UserDisplay()
+calculator_addition = CalculatorAdd
+
 # evaluate the chosen math operations
 math_operator = calculator_operator2.math_operator()
-    # if input is "Addition"
 
-    # if input is "Subtraction"
+# if input is "Addition"
+if math_operator.lower == "addition":
+    first_number = calculator_operator2.user_number
+    second_number = calculator_operator2.user_number
+# if input is "Subtraction"
 
-    # if input is "Multiplication"
+# if input is "Multiplication"
 
-    # if input is "Division"
+# if input is "Division"
 
 # ----------------------------------------------
 # ask user if wants to input again
