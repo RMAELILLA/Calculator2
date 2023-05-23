1ac692148a9a271aaf4622509a1ae9b637d22601
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-23 22:15:37 +0800

    new file for ud

---
+++ UserDisplay.py
@@ -0,0 +1,4 @@
+# ask user to choose math operations between "Addition", "Subtraction", "Multiplication", or "Division"
+class UserDisplay:
+    def math_operator(self):
+        input("Please choose what math operation you need: 'Addition', 'Subtraction', 'Multiplication' or 'Division': ")
