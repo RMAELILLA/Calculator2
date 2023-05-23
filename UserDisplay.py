c3f9ddfaec39b5414268d3ed56d9e47863b418a6
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-23 22:36:45 +0800

    let user input number

--- UserDisplay.py
+++ UserDisplay.py
@@ -1,4 +1,7 @@
 # ask user to choose math operations between "Addition", "Subtraction", "Multiplication", or "Division"
 class UserDisplay:
     def math_operator(self):
-        input("Please choose what math operation you need: 'Addition', 'Subtraction', 'Multiplication' or 'Division': ")
+        user_operator = input("Please choose what math operation you need: 'Addition', 'Subtraction', 'Multiplication' or 'Division': ")
+        return
+    def user_number(self):
+        first_number = float(input("Please enter number: "))
