57f99423947c9dd88cbdb769b3ef6f9e82186b49
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-24 10:06:32 +0800

    code per function to avoid lag

--- calculator_operator.py
+++ calculator_operator.py
@@ -9,3 +9,9 @@ class CalculatorOperator:
     def cal_multiply(self, first_number, second_number):
         multiplication = first_number * second_number
         return multiplication
+    def cal_divide(self, first_number, second_number):
+        try:
+            division = first_number / second_number
+            return division
+        except ZeroDivisionError:
+            print("Cannot process division, your divisor is zero")
