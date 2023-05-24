2274d07457db5b8945a6f8f97f1ce6c32e2cf743
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-24 10:57:18 +0800

    exception handling in math operators

--- calculator2.py
+++ calculator2.py
@@ -7,23 +7,25 @@ class Calculator2:
         self.user_display = UserDisplay()
         self.calculator_operator = CalculatorOperator()
     def run(self):
-        math_operator = self.user_display.math_operator()
-        math_operator = math_operator.lower()
-        first_number = self.user_display.user_number()
-        second_number = self.user_display.user_number()
+        math_operator = self.user_display.math_operator().lower()
 
-        if math_operator == "addition":
-            addition = self.calculator_operator.cal_add(first_number, second_number)
-            self.user_display.display_addition(addition)
-        elif math_operator == "subtraction":
-            subtraction = self.calculator_operator.cal_subtract(first_number, second_number)
-            self.user_display.display_subtraction(subtraction)
-        elif math_operator == "multiplication":
-            multiplication = self.calculator_operator.cal_multiply(first_number, second_number)
-            self.user_display.display_multiplication(multiplication)
-        elif math_operator == "division":
-            division = self.calculator_operator.cal_divide(first_number, second_number)
-            self.user_display.display_division(division)
+        if math_operator in ["addition", "subtraction", "multiplication", "division"]:
+            first_number = self.user_display.user_number()
+            second_number = self.user_display.user_number()
+
+            if math_operator == "addition":
+                addition = self.calculator_operator.cal_add(first_number, second_number)
+                self.user_display.display_addition(addition)
+            elif math_operator == "subtraction":
+                subtraction = self.calculator_operator.cal_subtract(first_number, second_number)
+                self.user_display.display_subtraction(subtraction)
+            elif math_operator == "multiplication":
+                multiplication = self.calculator_operator.cal_multiply(first_number, second_number)
+                self.user_display.display_multiplication(multiplication)
+            elif math_operator == "division":
+                division = self.calculator_operator.cal_divide(first_number, second_number)
+                self.user_display.display_division(division)
+            
         else:
             print("I don't understant your input, please choose one only in the four math operators.")
 
